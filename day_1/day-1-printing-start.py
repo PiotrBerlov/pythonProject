fb12b022c5e1e97e4ce498b8c9e8420c6123dd93
Piotr Maj <piotr.grzegorz.maj95@gmail.com>
2023-01-21 23:28:44 +0100

    1 task of day 1

---
+++ day_1/day-1-printing-start.py
@@ -0,0 +1,4 @@
+# Write a program in main.py that prints the same notes from the previous lesson using what you
+# have learnt about the Python print function.
+
+print("Hello " + input("What is your name? "))
